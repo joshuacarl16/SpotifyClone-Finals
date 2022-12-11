6fd33b339eee81046d03da6a9b59542c0778d935
joshuacarl16 <joshuacarl2001@gmail.com>
2022-12-11 20:16:09 +0800

    Merge branch 'main' of https://github.com/joshuacarl16/SpotifyClone-Finals

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -16,75 +16,71 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                     const Image(
-                        image: AssetImage("white_logo.png"),
-                        width: 40,
-                        height: 40,
-                      ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Image(
+            image: AssetImage("white_logo.png"),
+            width: 40,
+            height: 40,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          const Text(
+            "Millions of songs.",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(height: 10),
+          const Text(
+            "Free on Spotify.",
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 32),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Column(
+            children: [
+              //SIGN UP FREE BUTTON
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 105, 189, 137),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 120, vertical: 18),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                onPressed: () {},
+                child: const Text(
+                  'Sign up free',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w600),
+                ),
+              ),
 
-                      SizedBox(height: 30,),
+              SizedBox(
+                height: 10,
+              ),
 
-                      const Text(
-                        "Millions of songs.",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize:30,),
-                      ),
-
-                      SizedBox(height:10),
-
-                      const Text(
-                        "Free on Spotify.",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500, fontSize:32),
-                      ),
-
-                      SizedBox(height: 20,),
-                      Column(
-                        children: [
-                          //SIGN UP FREE BUTTON
-                       ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 105, 189, 137),
-                            padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 18),
-                            shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
-                            
-                          onPressed: () {},
-                          child: const Text('Sign up free',
-                         style: TextStyle(color: Colors.black,
-                         fontWeight: FontWeight.w600
-                         ),
-                         ),            
-                        ),
-
-                         SizedBox(height: 10,),
-
-
-                          //sign up google
-                          const SizedBox(height: 10,),
-                          IconButton(
-                            onPressed: () {
-                              Navigator.push(context, 
-                              MaterialPageRoute(
-                                builder: (context) => HomePage(),
-                              
-                              ),
-                              );
-                              const Text('Continue with Google');
-                            },
-                            
-                            icon: Image.asset("google.png") ,
-                             
-                            ),
-
-                       
-                        ],
-                      ),]
-        ),
-        
+              //sign up google
+              const SizedBox(
+                height: 10,
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Tabbar(),
+                    ),
+                  );
+                  const Text('Continue with Google');
+                },
+                icon: Image.asset("google.png"),
+              ),
+            ],
+          ),
+        ]),
       ),
     );
   }
