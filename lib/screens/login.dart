8919cad0b08a21de052eb05ec967c37ee8a77656
Julius Palalay <julius.palalay@usjr.edu.ph>
2022-12-11 20:09:55 +0800

    login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -16,7 +16,6 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-<<<<<<< HEAD
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -86,67 +85,6 @@ class _LoginState extends State<Login> {
                       ),]
         ),
         
-=======
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Image(
-            image: AssetImage("white_logo.png"),
-            width: 50,
-            height: 50,
-          ),
-          const Text(
-            "Millions of songs.",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
-          ),
-          const Text(
-            "Free on Spotify.",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
-          ),
-          Column(
-            children: [
-              //SIGN UP FREE BUTTON
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,
-                ),
-                onPressed: () {},
-                child: const Text('Sign up free'),
-              ),
-
-              SizedBox(
-                height: 10,
-              ),
-
-              //sign up google
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Sign up free',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Tabbar(),
-                    ),
-                  );
-                },
-                icon: Image.asset("google.png"),
-                tooltip: 'Continue with Google',
-              ),
-            ],
-          ),
-        ]),
->>>>>>> 87230371b61d9cab47d5dcac5edf3c210ba5ada4
       ),
     );
   }
