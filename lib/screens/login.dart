9d5b9432420a6782a06260dacfa1aac5dc373ec0
joshuacarl16 <joshuacarl2001@gmail.com>
2022-12-11 18:06:10 +0800

    connected navigator to login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'login.dart';
 
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Millions of songs.",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
+              ),
+              Text("Free on Spotify."),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+            ]),
+      ),
+    );
+  }
+}
